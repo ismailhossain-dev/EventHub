bb35e4c4116c79c9050f5b9d85bb85e3c3523968
sabbir <programmarsabbir@gmail.com>
2026-08-10 10:31:54 +0600

    eventhub-allevent-get-scucessfully

--- client/src/app/all-events/page.tsx
+++ client/src/app/all-events/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import RoomCard from '@/components/room/RoomCard/RoomCard';
+import Container from '@/components/shared/Container/Container';
+import Footer from '@/components/shared/Footer/Footer'
+import Navbar from '@/components/shared/Navbar/Navbar'
+import { Room } from '@/types/room';
+import axios from 'axios'
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const getAllEvents = async() => {
+  try {
+    const response = await axios.get(`${API_URL}/rooms`);
 
-function page() {
+  return response.data.data;
+  } catch (error) {
+    console.log("Failed to all events rooms", error)
+  }
+
+}
+
+interface eventProps {
+  rooms :  Room
+}
+
+async function  page () {
+
+  const rooms = await getAllEvents()
+  console.log("hello events", rooms)
   return (
-    <div>page</div>
+    <div>
+      <Navbar/>
+          <Container>
+           {/* parent div */}
+          {/* Header */}
+           <div className=""></div>
+           <div className='grid grid-cols-2 md:grid-cols-3 gap-4 l
+      md:gap-6'>
+            {
+              rooms.map((room)=> <RoomCard key={room.id} room={room}></RoomCard> )
+            }
+           </div>
+          </Container>
+      <Footer/>
+    </div>
   )
 }
 
